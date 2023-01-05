880df842aaa8c2473e6c8ce1edbffe8887e51564
Zuhaib Amjad <umar0423@gmail.com>
2023-01-05 18:22:25 +0500

    Create reverse method test in solver_spec file

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,5 +8,11 @@ describe Solver do
       result = 120
       expect(solver.factorial(arg)).to eql result
     end
+
+    it 'should reverse the string' do
+      string = 'hello'
+      reversed_str = 'olleh'
+      expect(solver.reverse(string)).to eql reversed_str
+    end
   end
 end
