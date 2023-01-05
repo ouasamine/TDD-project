3b7032f5ebaca7e55ff73000ceeb5be092f59cb8
ouasamine <ouas.amine@outlook.com>
2023-01-05 14:04:47 +0100

    Create rspec test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,11 @@
+
+describe Solver do 
+  context "Testing Solver methods" do
+    solver = Solver.new
+    it  "Test factorial method" do 
+      arg = 5
+      result = 120
+      expect(solver.factorial(arg)).to eql result
+    end
+  end
+end
