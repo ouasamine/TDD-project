d99285ba0605143e28c2bbfa3e394c1fab4cb7d6
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-01-05 14:35:50 +0100

    Add three test for the fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,9 @@
-require_relative '../solver.rb'
+require_relative '../solver'
 
-describe Solver do 
-  context "Testing Solver methods" do
+describe Solver do
+  context 'Testing Solver methods' do
     solver = Solver.new
-    it  "Test factorial method" do 
+    it 'Test factorial method' do
       arg = 5
       result = 120
       expect(solver.factorial(arg)).to eql result
@@ -13,6 +13,24 @@ describe Solver do
       string = 'hello'
       reversed_str = 'olleh'
       expect(solver.reverse(string)).to eql reversed_str
+    end
+
+    it 'Should return fizz argument is divisible by 3' do
+      arg = 3
+      result = 'fizz'
+      expect(solver.fizzbuzz(arg)).to eql result
+    end
+
+    it 'Should return buzz argument is divisible by 5' do
+      arg = 5
+      result = 'buzz'
+      expect(solver.fizzbuzz(arg)).to eql result
+    end
+
+    it 'Should return fizzbuzz argument is divisible by 3 and 5' do
+      arg = 15
+      result = 'fizzbuzz'
+      expect(solver.fizzbuzz(arg)).to eql result
     end
   end
 end
